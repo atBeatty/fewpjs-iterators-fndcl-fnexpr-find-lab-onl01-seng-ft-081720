098dd97139aea33a069e7714517d5ae26b86f893
atBeatty <adam.t.beatt@gmail.com>
2020-11-29 14:08:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
 
 function testFunc() {
   return "hi"
 }
+console.log(testFunc())
+
+function superbowlWin(arr){
+  let winExists = arr.find(row => row.result === "W")
+  debugger
+  
+  if (winExists) {
+    console.log(winExists.year)
+    debugger
+    return winExists.year
+  } 
+}
+
+superbowlWin(record)
